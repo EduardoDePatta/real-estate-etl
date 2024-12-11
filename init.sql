f46e9407ba79a199f7d756b37afbd53cf0921c88
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-12-11 00:19:37 -0300

    feat: add filters

--- init.sql
+++ init.sql
@@ -20,13 +20,13 @@ CREATE TABLE IF NOT EXISTS real_estate.captura (
 );
 
 INSERT INTO real_estate.portal (id, nome, url, observacoes)
-VALUES(1, 'Zap Imóveis', 'https://www.zapimoveis.com.br', 'Portal principal para imóveis de aluguel')
+VALUES(1, 'Zap Imóveis', 'https://glue-api.zapimoveis.com.br/v2/listings', 'Portal principal para imóveis de aluguel')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO real_estate.captura (id, portal_id, filtros, status, data_hora_inicio, data_hora_fim)
-VALUES(1, 1, '{"tipoNegocio": "venda"}'::jsonb, 'pendente', null, null)
+VALUES(1, 1, '{"portal": "ZAP", "business": "SALE", "listingType": "USED", "categoryPage": "RESULT", "includedFields": "search%28result%28listings%28listing%28legacyId%2Cid%2CexternalId%2Ctitle%2Cdescription%2Cportal%2CcontractType%2Caddress%2CpricingInfos%2Cbedrooms%2Cbathrooms%2CparkingSpaces%2CusableAreas%2CupdatedAt%2Clink%29%29%29%29"}'::jsonb, 'pendente', null, null)
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO real_estate.captura (id, portal_id, filtros, status, data_hora_inicio, data_hora_fim)
-VALUES(2, 1, '{"tipoNegocio": "aluguel"}'::jsonb, 'pendente', null, null)
+VALUES(2, 1, '{"portal": "ZAP", "business": "RENTAL", "listingType": "USED", "categoryPage": "RESULT", "includedFields": "search%28result%28listings%28listing%28legacyId%2Cid%2CexternalId%2Ctitle%2Cdescription%2Cportal%2CcontractType%2Caddress%2CpricingInfos%2Cbedrooms%2Cbathrooms%2CparkingSpaces%2CusableAreas%2CupdatedAt%2Clink%29%29%29%29"}'::jsonb, 'pendente', null, null)
 ON CONFLICT (id) DO NOTHING;
