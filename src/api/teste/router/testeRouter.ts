2def4d969ff2a6b8290b542f6888b8cc719c426b
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-12-10 21:30:40 -0300

    refactor: rm test service

--- src/api/teste/router/testeRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { ServiceFactory } from '../../../BaseService/ServiceFactory'
-import { TesteService } from '../services/TesteService'
-
-const testeRouter = Router()
-
-testeRouter.get('/', ServiceFactory.getHandler(TesteService))
-
-export { testeRouter }
