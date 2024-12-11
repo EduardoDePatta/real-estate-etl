7d9caa9c1ffb01bec2bac5e3c9905fef343e0ff0
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-12-10 21:28:58 -0300

    feat: update environments

--- src/environments.ts
+++ src/environments.ts
@@ -3,10 +3,11 @@ dotenv.config()
 
 export default {
   NODE_ENV: process.env.NODE_ENV || 'dev',
-  DB_HOST: process.env.DB_HOST || '',
-  DB_USER: process.env.DB_USER || '',
-  DB_DATABASE: process.env.DB_DATABASE || '',
-  DB_PASSWORD: process.env.DB_PASSWORD || '',
-  DB_PORT: Number(process.env.DB_PORT) ?? 5432,
-  PORT: Number(process.env.PORT) ?? 5000
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_USER: process.env.DB_USER || 'postgres',
+  DB_DATABASE: process.env.DB_DATABASE || 'real_estate',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'postgres',
+  DB_PORT: Number(process.env.DB_PORT) ?? 5433,
+  PORT: Number(process.env.PORT) ?? 5000,
+  ELASTIC_URL: process.env.ELASTIC_URL || 'http://localhost:9200'
 }
